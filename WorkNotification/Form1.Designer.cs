f83e7cb0802f7fe8846f2680b5fb393b68796f41
Blackfire363 <blackfire363@yahoo.co.uk>
2015-08-08 17:21:51 +0100

    Starting the Project with a Icon and setting up the notifcation area when the program is idle

---
+++ WorkNotification/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WorkNotification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuNotificationTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.restoreApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuNotificationTray.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuNotificationTray;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Work Notifications";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuNotificationTray
+            // 
+            this.contextMenuNotificationTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restoreApplicationToolStripMenuItem,
+            this.closeApplicationToolStripMenuItem});
+            this.contextMenuNotificationTray.Name = "contextMenuNotificationTray";
+            this.contextMenuNotificationTray.Size = new System.Drawing.Size(178, 48);
+            // 
+            // restoreApplicationToolStripMenuItem
+            // 
+            this.restoreApplicationToolStripMenuItem.Name = "restoreApplicationToolStripMenuItem";
+            this.restoreApplicationToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.restoreApplicationToolStripMenuItem.Text = "Restore Application";
+            this.restoreApplicationToolStripMenuItem.Click += new System.EventHandler(this.restoreApplicationToolStripMenuItem_Click);
+            // 
+            // closeApplicationToolStripMenuItem
+            // 
+            this.closeApplicationToolStripMenuItem.Name = "closeApplicationToolStripMenuItem";
+            this.closeApplicationToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.closeApplicationToolStripMenuItem.Text = "Close Application";
+            this.closeApplicationToolStripMenuItem.Click += new System.EventHandler(this.closeApplicationToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Working Timetable";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuNotificationTray.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuNotificationTray;
+        private System.Windows.Forms.ToolStripMenuItem restoreApplicationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeApplicationToolStripMenuItem;
+    }
+}
+
---
+++ WorkNotification/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkNotification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+            {
+                Hide();
+            }
+        }
+
+       private void restoreApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+       {
+           this.Show();
+           this.WindowState = FormWindowState.Normal;
+       }
+
+       private void closeApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+       {
+           Application.Exit();
+       }
+
+    }
+}
