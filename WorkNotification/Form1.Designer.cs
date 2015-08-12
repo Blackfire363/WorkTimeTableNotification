fd737054bbb24a6f9e350c7645938089d1b6794f
Blackfire363 <blackfire363@yahoo.co.uk>
2015-08-12 13:42:39 +0100

    Adding the new method that will check what hours you will work

--- WorkNotification/Form1.Designer.cs
+++ WorkNotification/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.contextMenuNotificationTray = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.restoreApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.closeApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_testbubble = new System.Windows.Forms.Button();
             this.contextMenuNotificationTray.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -67,11 +69,32 @@
             this.closeApplicationToolStripMenuItem.Text = "Close Application";
             this.closeApplicationToolStripMenuItem.Click += new System.EventHandler(this.closeApplicationToolStripMenuItem_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insert the days you are working this week";
+            // 
+            // btn_testbubble
+            // 
+            this.btn_testbubble.Location = new System.Drawing.Point(13, 30);
+            this.btn_testbubble.Name = "btn_testbubble";
+            this.btn_testbubble.Size = new System.Drawing.Size(75, 23);
+            this.btn_testbubble.TabIndex = 2;
+            this.btn_testbubble.Text = "Bubble Text";
+            this.btn_testbubble.UseVisualStyleBackColor = true;
+            this.btn_testbubble.Click += new System.EventHandler(this.btn_testbubble_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.btn_testbubble);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -82,6 +105,7 @@
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.contextMenuNotificationTray.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -91,6 +115,8 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuNotificationTray;
         private System.Windows.Forms.ToolStripMenuItem restoreApplicationToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem closeApplicationToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_testbubble;
     }
 }
 
--- WorkNotification/Form1.cs
+++ WorkNotification/Form1.cs
@@ -15,6 +15,12 @@ namespace WorkNotification
         public Form1()
         {
             InitializeComponent();
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
 
         private void Form1_Resize(object sender, EventArgs e)
@@ -22,6 +28,7 @@ namespace WorkNotification
             if (FormWindowState.Minimized == WindowState)
             {
                 Hide();
+                notifyIcon1.Visible = true;
             }
         }
 
@@ -29,6 +36,7 @@ namespace WorkNotification
        {
            this.Show();
            this.WindowState = FormWindowState.Normal;
+           notifyIcon1.Visible = false;
        }
 
        private void closeApplicationToolStripMenuItem_Click(object sender, EventArgs e)
@@ -36,10 +44,28 @@ namespace WorkNotification
            Application.Exit();
        }
 
-       private void Form1_Load(object sender, EventArgs e)
+       private void btn_testbubble_Click(object sender, EventArgs e)
        {
-
+           DisplayWorkingHours();
        }
+       
+        private void DisplayWorkingHours()
+        {
+            DateTime thisDay = DateTime.Today;
+            String dayName = thisDay.DayOfWeek.ToString();
 
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipText = "Are you blind? The day is: " + dayName;
+            this.notifyIcon1.BalloonTipTitle = dayName;
+            this.notifyIcon1.ShowBalloonTip(30000);
+
+            if (dayName == "Sunday")
+            {
+
+            }
+            else if (dayName == "Monday")
+            {
+            }
+        }
     }
 }
